5e51e081809b2a36269eee8d66c920e82fec6214
ThirdVector3 <thirdvector3@gmail.com>
2025-06-17 15:14:57 +0300

    remove test

--- Assets/YObject/Scripts/CoreComponents/Editor/YGameManagerEditor.cs
+++ Assets/YObject/Scripts/CoreComponents/Editor/YGameManagerEditor.cs
@@ -37,7 +37,7 @@ public class YGameManagerEditor : Editor
             yGameManager.CreateSampleLevel();
         }
 
-        EditorGUILayout.PropertyField(playerNameProperty, new GUIContent("Player Name"));
+        //EditorGUILayout.PropertyField(playerNameProperty, new GUIContent("Player Name"));
 
         serializedObject.ApplyModifiedProperties();
     }
